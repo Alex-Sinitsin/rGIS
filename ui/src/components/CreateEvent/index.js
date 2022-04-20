a1bd9f2de2d551fb5047454a4f7ab5c80515260b
Alexey <lehych99@mail.ru>
2022-04-20 17:11:35 +0500

    Добавил комментарий

--- ui/src/components/CreateEvent/index.js
+++ ui/src/components/CreateEvent/index.js
@@ -28,7 +28,7 @@ const CreateEvent = () => {
             title: 'Что-то другое',
         },
     ];
-
+    //TODO: Добавить функцию для вывода содержимого пошагового действия
     return (
         <>
             <Steps current={current}>
